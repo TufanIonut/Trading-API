872db34bca1eb166c08c9fbdfa62136ed2bb93bb
Tufan Ionut-Petrisor <117408976+TufanIonut@users.noreply.github.com>
2024-04-27 11:46:24 +0300

    Add project files.

---
+++ Trading-API/MyConfigServiceCollection.cs
@@ -0,0 +1,16 @@
+﻿using Trading_API.Infrastructure;
+using Trading_API.Interfaces;
+
+namespace Trading_API
+{
+    public static class MyConfigServiceCollection
+    {
+        public static IServiceCollection AddMyDependencyGroup(this IServiceCollection services)
+        {
+            //Database Connection
+            services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
+            //----------------------------------------------
+            return services;
+        }
+    }
+}
